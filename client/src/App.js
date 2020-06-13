8710d8007315fc216bf9b8e0be7c80f6bf4080db
k w <kevinwng0@gmail.com>
2020-06-13 16:04:24 -0700

    starting out

--- client/src/App.js
+++ client/src/App.js
@@ -4,27 +4,80 @@ import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import LandingPage from './components/pages/LandingPage/LandingPage.js';
-import Blog from './components/pages/Blog/Blog.js';
-import WriteArticle from './components/pages/WriteArticle/WriteArticle.js';
+import Favorites from './components/pages/Favorites/Favorites.js';
+import Recent from './components/pages/Recent/Recent.js';
 
-function App () {
+function App() {
   return (
     <div className="App">
+
       <nav className="App-navigation">
-        <h1 className="App-title">MERN Starter</h1>
-        <Link to="/">Welcome</Link>
-        <Link to="/blog/">Blog</Link>
-        <Link to="/write/">Write Article</Link>
+        <h1 className="App-title">Quarantine & Chill</h1>
+        {/* <Link to="/">Welcome</Link> */}
+        <Link to="/favorites/">Favorites</Link>
+        <br />
+        <Link to="/recent/">Recent</Link>
       </nav>
 
       <div className="App-mainContent">
         <Switch>
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/blog/' component={Blog} />
-          <Route exact path='/write/' component={WriteArticle} />
+          {/* <Route exact path='/' component={LandingPage} /> */}
+          <Route exact path='/favorites/' component={Favorites} />
+          <Route exact path='/recent/' component={Recent} />
         </Switch>
       </div>
+      <div className="movie-img"></div>
 
+      <label for="Genre">Genre</label>
+      <select name="Genre" id="Genre">
+        <option value="Action">Action</option>
+        <option value="Adventure">Adventure</option>
+        <option value="Animation">Animation</option>
+        <option value="Comedy">Comedy</option>
+      </select>
+      <br />
+      <label for="Rating">Rating</label>
+      <select name="Rating" id="Rating">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+      <br />
+      <label for="Length">Length</label>
+      <select name="Length" id="Length">
+        <option value="60">60</option>
+        <option value="90">90</option>
+        <option value="120">120</option>
+        <option value="150">150</option>
+      </select>
+
+      {/* @@@@@@@@@@@@@@@@@@@ */}
+
+      <div className="food-img"></div>
+
+      <label for="Cuisine Type">Cuisine Type</label>
+      <select name="Cuisine Type" id="Cuisine Type">
+        <option value="African">African</option>
+        <option value="American">American</option>
+        <option value="British">British</option>
+        <option value="Caribbean">Caribbean</option>
+      </select>
+      <br />
+      <label for="Meal Type">Meal Type</label>
+      <select name="Meal Type" id="Meal Type">
+        <option value="Dinner">Dinner</option>
+        <option value="Breakfast">Breakfast</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+      <br />
+      <label for="Food Allergies">Food Allergies</label>
+      <select name="Food Allergies" id="Food Allergies">
+        <option value="Gluten Free">Gluten Free</option>
+        <option value="Ketogenic">Ketogenic</option>
+        <option value="Vegetarian">Vegetarian</option>
+      </select>
     </div>
   );
 }
--- client/src/components/pages/LandingPage/LandingPage.js
+++ client/src/components/pages/LandingPage/LandingPage.js
@@ -1,21 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
 import './LandingPage.css';
 
-function LandingPage() {
+function Blog() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  function fetchPosts() {
+    console.log('Fetching data from API');
+    fetch('/api/mongodb/blogposts/')
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got data back', data);
+        setBlogPosts(data);
+      });
+  }
+
+  function deleteArticle(documentId) {
+    console.log('Sending DELETE for', documentId);
+    // Do the DELETE, using "?_id=" to specify which document we are deleting
+    fetch('/api/mongodb/blogposts/?_id=' + documentId, {
+      method: 'DELETE',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got this back', data);
+
+        // Call method to refresh data
+        fetchPosts();
+      });
+  }
+
+  function voteArticle(article) {
+    let newVoteCount = article.voteCount;
+
+    // Increase the vote count 
+    if (!newVoteCount) {
+      newVoteCount = 1;
+    } else {
+      newVoteCount++;
+    }
+
+    const formData = {
+      voteCount: newVoteCount,
+    };
+
+    // Do the PUT, using "?_id=" to specify which document we are affecting
+    const documentId = article._id;
+    fetch('/api/mongodb/blogposts/?_id=' + documentId, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Got this back', data);
+
+        // Call method to refresh data
+        fetchPosts();
+      });
+  }
+
+  // Invoke fetchPosts on initial load
+  useEffect(fetchPosts, []);
+
   return (
-    <div className="LandingPage">
-      <header className="LandingPage-header">
-        <p>
-          Kickstart Coding - Frontend React<br />
-          MERN Starter Project
-        </p>
-        <Link to="/blog/">Blog</Link>
-        <Link to="/write/">Write article</Link>
-      </header>
+    <div className="Blog">
+      <h1>Blog</h1>
+      {
+        blogPosts.map((post, index) => (
+          <div className="Blog-article" key={post._id}>
+
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+
+            <div className="Blog-articleActions">
+              <div onClick={() => deleteArticle(post._id)}>
+                <span alt="delete this">🗑</span>
+              </div>
+              <div onClick={() => voteArticle(post)}>
+                <span alt="upvote this">⬆ {post.voteCount}</span>
+              </div>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default LandingPage;
+export default Blog;
