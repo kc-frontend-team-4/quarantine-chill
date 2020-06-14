6715dcc7cf093bc19687864b504573cf0f79c487
k w <kevinwng0@gmail.com>
2020-06-13 19:47:54 -0700

    add components

---
+++ client/src/components/pages/Results/Results.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './Results.css';
+function Results() {
+  return (
+    <div>
+      <button>Start Over</button>
+      <div className="movie-img">Movie</div>
+      <div className="food-img">Food</div>
+      <button>Love it!</button>
+      <button>Give me another</button>
+    </div>
+  )
+}
+export default Results;
