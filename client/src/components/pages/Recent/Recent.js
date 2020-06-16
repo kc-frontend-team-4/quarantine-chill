3bc46f1e5256134c782ad6fbff3282ef27bd0077
Patrick Ware <patrick.r.ware@gmail.com>
2020-06-16 08:56:58 -0700

    added navbar jsx, added recents jsx

--- client/src/components/pages/Recent/Recent.js
+++ client/src/components/pages/Recent/Recent.js
@@ -4,20 +4,43 @@ import './Recent.css';
 
 function Recent() {
   return (
-    <div >
-      <p>Recent</p>
-      <p>★
-        <div className="food-img">Food</div>
-        <div className="movie-img">Movie</div>
-      </p>
-      <p>★
-        <div className="food-img">Food</div>
-        <div className="movie-img">Movie</div>
-      </p>
-      <p>★
-        <div className="food-img">Food</div>
-        <div className="movie-img">Movie</div>
-      </p>
+    <div>
+        <main>
+            <div className="movie-column">
+                <div className="main-heading" style={{right: "-50px"}}>Recent</div>
+                <div className="movie-selections">
+                    <div className="star-and-image">
+                        <div className="star">⭐<span className="count">4</span></div>
+                        <div className="movie-image">
+                        </div>
+                    </div>
+                    <div className="star-and-image">
+                        <div className="star">⭐<span className="count">6</span></div>
+                        <div className="movie-image">
+                        </div>
+                    </div>
+                    <div className="star-and-image">
+                        <div className="star">⭐<span className="count">7</span></div>
+                        <div className="movie-image">
+                        </div>
+                    </div>
+            </div>
+            </div>
+            <div className="recipe-column">
+                <div className="recipe-selections">
+                <div className="recipe-image"></div>
+                <div className="recipe-image"></div>
+                <div className="recipe-image"></div>
+            </div>
+            </div>
+        </main>
+        <div className="pagination">
+            <div className="page-square"></div>
+            <div className="page-square"></div>
+            <div className="page-square"></div>
+            <div className="page-square"></div>
+            <div className="page-square"></div>
+        </div>
     </div>
   );
 }
