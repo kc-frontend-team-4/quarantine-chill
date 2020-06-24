3303e27ad4997a5931f51ef7f8f1d2d58c5a5d34
Tim Myers <tdmsportplayer@yahoo.com>
2020-06-23 23:42:01 -0700

    Results redesign WIP

--- client/src/components/pages/Results/Results.js
+++ client/src/components/pages/Results/Results.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Results.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import imdb from '../../../imdb.png'
+
 function Results(props) {
   useEffect(() => {
     console.log("recipe info found!")
@@ -13,23 +15,32 @@ function Results(props) {
           <div className="start-over">
             <Link to="/">Start Over</Link>
           </div>
-          <div className="movie-selections">
+          <div className="movie-selections-results">
             <div className="movie-image">
               <img src={`https://image.tmdb.org/t/p/w200${props.randomedMovie.poster_path}`}></img>
             </div>
-
-            <a target="_blank" href={'https://www.imdb.com/title/' + props.imdbId}> <p className="movie-name">{props.randomedMovie.title || props.randomedMovie.name}</p></a>
+            <div className="movie-information">
+              <a target="_blank" href={'https://www.imdb.com/title/' + props.imdbId}> <p className="movie-name">{props.randomedMovie.title || props.randomedMovie.name}</p></a>
+              <p>{props.movieOverview}</p>
+            <div class="movie-link-container">
+              <div class="movie-link">
+                <a target="_blank" href={props.recipeInfo.url}>Check it out on <img src={imdb} /></a>
+              </div>
+            </div>
+            </div>
           </div>
-          <h2>{props.movieOverview}</h2>
+          
 
         </div>
         <div className="recipe-column">
-          <div className="recipe-selections">
+          <div className="recipe-selections-results">
             <div className="recipe-image">
               <img className="recipe-image-item" src={props.recipeInfo.img} />
             </div>
-            <a target="_blank" href={props.recipeInfo.url}> <p className="recipe-name">{props.recipeInfo.name}</p></a>
-            <p>Cook time is {props.recipeInfo.cooktime} minutes.</p>
+            <div className="recipe-information">
+              <p className="recipe-name">{props.recipeInfo.name}</p>
+              <p>Cook time is {props.recipeInfo.cooktime} minutes.</p>
+            </div>
           </div>
         </div>
         <button style={{ alignSelf: "flex-start" }}>Love It!</button>
