09475e9dd7a9c345cad11cdda807571d3c1bdfe3
Patrick Ware <patrick.r.ware@gmail.com>
2020-06-26 18:27:14 -0700

    reverse array functionality added to show most recent results first

--- client/src/components/pages/Recent/Recent.js
+++ client/src/components/pages/Recent/Recent.js
@@ -14,7 +14,7 @@ function Recent() {
       .then(response => response.json())
       .then(data => {
         console.log('Got data back', data);
-        setPairings(data);
+        setPairings(data.reverse());
       });
   }
 
@@ -33,25 +33,7 @@ function Recent() {
                     randomedMovieTitle={item.randomedMovie.title}
                     randomedMovieName={item.randomedMovie.name}
                     imdbId={item.imdbId}
-                  />
-                /*<div className="movie-selections-recent">
-                  <div className="movie-image">
-                    <img src={`https://image.tmdb.org/t/p/w200${item.randomedMovie.poster_path}`}></img>
-                  </div>
-                  <div className="movie-information">
-                    <p className="movie-name">{item.randomedMovie.title || item.randomedMovie.name}</p>
-                    <div className="movie-link-container-recent">
-                      <div className="movie-link-recent">
-                        <a 
-                          className="imdb-text" 
-                          target="_blank" 
-                          href={'https://www.imdb.com/title/' + item.imdbId}
-                        >Check it out on <img className="imdb-img" src={imdb} />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>*/                   
+                  />                
               ))}
             </div>
             </div>
