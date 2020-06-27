44eaf3f60a10169f1b3528022fcd037b5c747b96
Patrick Ware <patrick.r.ware@gmail.com>
2020-06-27 12:23:16 -0700

    recent spinner working

--- client/src/components/pages/Recent/Recent.js
+++ client/src/components/pages/Recent/Recent.js
@@ -3,8 +3,11 @@ import './Recent.css';
 import RecentCard from '../../RecentCard/RecentCard.js'
 import imdb from '../../../imdb.png'
 
+import { css } from "@emotion/core";
+import FadeLoader from "react-spinners/FadeLoader";
+
 function Recent() {
-  const [pairings, setPairings] = useState([])
+  const [pairings, setPairings] = useState(null)
   const [page, setPage] = useState(1);
 
   // Go to next page
@@ -36,32 +39,43 @@ function Recent() {
 
   useEffect(fetchPairings, [])
 
-  return (
-    <div>
-          <div className="recent-page"> 
-            <div style={{marginTop:"120px"}}>
-              {
-                pairings.slice(page*4-4, page*4)
-                  .map((item) => (
-                    <RecentCard
-                      poster_path={item.randomedMovie.poster_path}
-                      randomedMovieTitle={item.randomedMovie.title}
-                      randomedMovieName={item.randomedMovie.name}
-                      imdbId={item.imdbId}
-                      recipeImg={item.recipeInfo.img}
-                      recipeName={item.recipeInfo.name}
-                      recipeUrl={item.recipeInfo.url}
-                    /> 
-                ))}
-              <div className="page-nav">
-                <div className="page-change-prev" onClick={goToPreviousPage}>⬅ Previous Page </div>
-                <div className="page-change-next" onClick={goToNextPage}>Next Page ➡</div>
+  if (pairings === null) {
+    return (
+      <div className="recent-loader">      
+        <FadeLoader
+          size={2500}
+          color={'#000000'}
+        />
+      </div>  
+    );
+  }
+    else{
+      return (
+        <div>
+              <div className="recent-page"> 
+                <div style={{marginTop:"120px"}}>
+                  {
+                    pairings.slice(page*4-4, page*4)
+                      .map((item) => (
+                        <RecentCard
+                          poster_path={item.randomedMovie.poster_path}
+                          randomedMovieTitle={item.randomedMovie.title}
+                          randomedMovieName={item.randomedMovie.name}
+                          imdbId={item.imdbId}
+                          recipeImg={item.recipeInfo.img}
+                          recipeName={item.recipeInfo.name}
+                          recipeUrl={item.recipeInfo.url}
+                        /> 
+                    ))}
+                  <div className="page-nav">
+                    <div className="page-change-prev" onClick={goToPreviousPage}>⬅ Previous Page </div>
+                    <div className="page-change-next" onClick={goToNextPage}>Next Page ➡</div>
+                  </div>
+                  <div className="current-page"> Page {page} of {Math.ceil(pairings.length/4)} </div>
               </div>
-              <div className="current-page"> Page {page} of {Math.ceil(pairings.length/4)} </div>
-          </div>
+            </div>
         </div>
-    </div>
-  );
-}
+      );
+    }}
 
 export default Recent;
